8c5fc542a6f4e9cb13c03294558ce652996a064f
Erick Felix <83036518+Computerick@users.noreply.github.com>
2025-05-27 21:50:10 -0300

    licao 9 Read.me

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -161,7 +161,7 @@ describe('Central de Atendimento ao Cliente TAT', () => { //define a suite de te
         expect(input[0].files[0].name).to.equal('example.json')
       })
  })
-it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
   cy.fixture('example.json').as('sampleFile')
   cy.get('#file-upload')
     .selectFile('@sampleFile')
@@ -169,4 +169,19 @@ it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um ali
       expect(input[0].files[0].name).to.equal('example.json')
     })
  })
+
+ it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+  cy.contains('a','Política de Privacidade')
+    .should('have.attr', 'href', 'privacy.html')
+    .and('have.attr', 'target', '_blank')
+ })
+
+ it('acessar a página da política de privacidade removendo o target e então clicando no link', () => {
+  cy.contains('a','Política de Privacidade')
+    .invoke('removeAttr', 'target' )
+    .click()
+  cy.contains('h1', 'CAC TAT - Política de Privacidade')
+    .should('be.visible')
+ })
+ 
 })
---
+++ cypress/e2e/privacyPolicy.cy.js
@@ -0,0 +1,7 @@
+
+it('testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./src/privacy.html')
+    cy.contains('h1', 'CAC TAT - Política de Privacidade',  {timeout: 2000})
+        .should('be.visible')
+    cy.contains('p','Talking About Testing').should('be.visible')
+ })
